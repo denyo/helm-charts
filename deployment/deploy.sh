10beb2fb025e6e6a7f83b6d5aac951096ee40d87
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-09 09:28:34 +0200

    ci: add deploy.sh and enable deploy stage

---
+++ deployment/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(cd $(dirname ${0}) && pwd)"
+NAMESPACE=${1:-helm-repository}
+HELM_RELEASE=${2:-helm-repository}
+
+kubectl create ns $NAMESPACE || true
+helm -n ${NAMESPACE} upgrade --install ${HELM_RELEASE} ${SCRIPT_DIR}/../nginx -f ${SCRIPT_DIR}/values.yaml
