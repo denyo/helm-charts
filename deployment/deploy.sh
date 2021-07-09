f59733a9aa74c567a087dd991fd09f68679446a0
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-09 10:57:10 +0200

    ci: fix path to nginx chart in deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$(cd $(dirname ${0}) && pwd)"
 NAMESPACE=${1:-helm-repository}
 HELM_RELEASE=${2:-helm-repository}
 
-helm -n ${NAMESPACE} upgrade --install ${HELM_RELEASE} ${SCRIPT_DIR}/../nginx -f ${SCRIPT_DIR}/values.yaml --set image.tag=$CI_COMMIT_SHA
+helm -n ${NAMESPACE} upgrade --install ${HELM_RELEASE} ${SCRIPT_DIR}/../charts/nginx -f ${SCRIPT_DIR}/values.yaml --set image.tag=$CI_COMMIT_SHA
