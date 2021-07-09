a19185377cfea282002d45ae6742f03f3a33c828
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-09 10:53:11 +0200

    chore: move all charts into charts folder, adjust package-charts.sh

--- package-charts.sh
+++ package-charts.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-all_dirs=(./*/)
-chart_dirs=${all_dirs[*]}
-
+chart_dirs=(./charts/*/)
 dist=./dist/
-delete=($dist "./docker/")
-for del in ${delete[@]}; do
-    chart_dirs=("${chart_dirs[@]/$del/}")
-done
 
-for dir in $chart_dirs; do
+for dir in ${chart_dirs[*]}; do
     echo "Packing chart at: $dir"
     helm package $dir -d $dist
 done
