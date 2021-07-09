01b2d49e7b4ab2810d2905e0266f931cb2b2d836
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-09 10:10:34 +0200

    ci: remove k8s namespace creation in deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,5 +4,4 @@ SCRIPT_DIR="$(cd $(dirname ${0}) && pwd)"
 NAMESPACE=${1:-helm-repository}
 HELM_RELEASE=${2:-helm-repository}
 
-kubectl create ns $NAMESPACE || true
 helm -n ${NAMESPACE} upgrade --install ${HELM_RELEASE} ${SCRIPT_DIR}/../nginx -f ${SCRIPT_DIR}/values.yaml --set image.tag=$CI_COMMIT_SHA
